62d564b6a8b62613edb1b861dd796f14ca24114d
Wenbin Liu <liuwenbin.lwb@gmail.com>
2018-07-01 02:18:25 +0800

    add base database operate

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # timeline
+
+DateBase: PostgresSQL
--- server/main.go
+++ server/main.go
@@ -2,8 +2,54 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/jackc/pgx"
 )
 
 func main() {
-	fmt.Println("hello world")
+	const host string = "localhost"
+	const database string = "timeline"
+	port, err := strconv.ParseUint(os.Getenv("PGPORT"), 10, 16)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	user := os.Getenv("PGUSER")
+	password := os.Getenv("PGPASSWORD")
+
+	connconfig := pgx.ConnConfig{
+		Host:     host,
+		Port:     uint16(port),
+		Database: database,
+		User:     user,
+		Password: password,
+	}
+
+	connPoolConfig := pgx.ConnPoolConfig{
+		ConnConfig: connconfig,
+	}
+
+	connPool, err := pgx.NewConnPool(connPoolConfig)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	result, err := connPool.Exec(`CREATE TABLE events (
+		subject text,
+		description text
+	);`)
+
+	if err != nil && err.(pgx.PgError).Code != "42P07" {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	connPool.Exec("insert into events(subject, description) values($1, $2)", "Test Subject", "This is a test subject")
+
+	fmt.Println(result)
+
+	fmt.Println(connPoolConfig)
 }
